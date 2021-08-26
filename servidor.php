8df4a4b8e3fe112cac0f84a047f9cfe4c89f4c78
GERALD ANTONIO RAMIREZ FONSECA <gerald.ramirezfonseca@ucr.ac.cr>
2021-08-26 08:53:57 -0600

    editserver

--- servidor.php
+++ servidor.php
@@ -1,2 +1,2 @@
    <!---http://localhost:3000 | https://grfixurdivice.000webhostapp.com | https://emra-store-cr.azurewebsites.net-->
-   <?php $server = "http://localhost:3000"; ?>
+   <?php $server = "https://emra-store-cr.azurewebsites.net"; ?>
